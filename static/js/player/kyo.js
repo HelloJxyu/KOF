def0192210b5aae4b83e6680a98ef61359de50f1
HelloJxyu <2278605157@qq.com>
2022-12-15 09:46:47 +0800

    修改图片链接为地址引用

--- static/js/player/kyo.js
+++ static/js/player/kyo.js
@@ -12,7 +12,22 @@ export class Kyo extends Player {
         let off_set = [0, -22, -22, -140, 0, 0, 0];
         for (let i = 0; i < 7; i++) {
             let gif = GIF();
-            gif.load(`/static/images/player/kyo/${i}.gif`);
+            if (i == 0) {
+                gif.load(`https://s2.loli.net/2022/12/15/f17bnRVKXlsJvUQ.gif`);
+            } else if (i === 1) {
+                gif.load(`https://s2.loli.net/2022/12/15/yGja1lIW3DzwVTx.gif`);
+            } else if (i === 2) {
+                gif.load(`https://s2.loli.net/2022/12/15/XxMQBqdKvY2Pr8T.gif`);
+            } else if (i === 3) {
+                gif.load(`https://s2.loli.net/2022/12/15/nhKIyLoPY6uSO29.gif`);
+            } else if (i === 4) {
+                gif.load(`https://s2.loli.net/2022/12/15/TcWMkjVx4tR8FC5.gif`);
+            } else if (i === 5) {
+                gif.load(`https://s2.loli.net/2022/12/15/IBFLsQoPTZWOR9v.gif`);
+            } else {
+                gif.load(`https://s2.loli.net/2022/12/15/x7Rf3lQ6kmnciC5.gif`);
+            }
+            // gif.load(`../../images/player/kyo/${i}.gif`);
             this.animations.set(i, {
                 gif,
                 frame_cnt: 0,
